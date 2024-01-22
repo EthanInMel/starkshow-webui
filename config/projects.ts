d1476e1489b53538f0f6b6633f9e265921932964
Yi Song (Ethan) <ethaninmel@gmail.com>
2024-01-22 19:50:41 +0800

    projects

--- config/projects.ts
+++ config/projects.ts
@@ -1,133 +1,46 @@
 export default [
     {
-        id: 0,
-        name: 'WatchLiq',
-        brief: 'ERC20 token that powers watch liquidity lending for watch-backed nfts built by watches.io',
-        event: 'ETHGlobal New York',
+        id: "1",
+        name: 'Stealth Addresses Contract Wallet',
+        brief: 'Stealth Addresses Contract Wallet: EIP-5564 In Cairo',
+        event: 'Starknet Hacker House, Chengdu',
         cardImage: '/images/projects/10k.jpeg',
         bgImage: '/images/projects/10k.jpeg',
-        description: `Introducing WatchLiQ Token (WATCH), the revolutionary ERC-20 token designed to redefine the landscape of watch-based Non-Fungible Tokens (NFTs) by fueling lending protocols tailored specifically for these unique digital assets. WatchLiQ Token is your top choice for your hackathon submission, offering an innovative solution that paves the way for new opportunities for NFT enthusiasts and collectors, empowering them to maximize the utility of their treasured timepiece-based digital assets.
-
-        Key Features and Functionality:
-        
-        Lending Empowerment: WatchLiQ Token (WATCH) serves as the backbone of lending platforms exclusively dedicated to watch-based NFTs. Users can utilize their valuable NFT watches as collateral to obtain WATCH, unlocking liquidity without the need to part with their cherished digital assets.
-        
-        Decentralized Governance: At WatchLiQ Token, we believe in community-driven decision-making. WATCH holders play a pivotal role in shaping lending protocols through on-chain governance, ensuring a transparent and democratic ecosystem.
-        
-        Immutable Ownership: Built on the robust Ethereum blockchain, WatchLiQ Token guarantees the security and perpetual ownership rights of your watch NFTs. Your digital assets remain securely under your control throughout the lending process.
-        
-        Interest Earnings: Lenders can earn competitive interest rates by providing WATCH to the lending pool, offering an enticing opportunity for token holders to generate passive income while supporting the lending ecosystem.
-        
-        Cross-Compatibility: WatchLiQ Token (WATCH) seamlessly integrates with existing DeFi platforms and wallets, ensuring easy accessibility and tradability on various decentralized exchanges and marketplaces.
-        
-        Smart Contract Security: Our smart contracts are rigorously audited and engineered with the highest standards of security and reliability, prioritizing the protection of user assets and transactions.
-        
-        Scalability and Future-Proofing: WatchLiQ Token (WATCH) is built to scale, allowing for the seamless integration of new features and enhancements as the watch-based NFT lending space continues to evolve.`,
+        description: '/md/projects/Project_5564_in_Cairo_in_CD_hacker_house.md',
         source: '',
         category: 'AA'
     },
     {
-        id: 1,
-        name: 'WatchLiq',
-        brief: 'ERC20 token that powers watch liquidity lending for watch-backed nfts built by watches.io',
-        event: 'ETHGlobal New York',
+        id: "2",
+        name: 'Action Market',
+        brief: 'Starknet contract as an executor to execute a certain on-chain transaction',
+        event: 'Starknet Hacker House, Chengdu',
         cardImage: '/images/projects/10k.jpeg',
         bgImage: '/images/projects/10k.jpeg',
-        description: `Introducing WatchLiQ Token (WATCH), the revolutionary ERC-20 token designed to redefine the landscape of watch-based Non-Fungible Tokens (NFTs) by fueling lending protocols tailored specifically for these unique digital assets. WatchLiQ Token is your top choice for your hackathon submission, offering an innovative solution that paves the way for new opportunities for NFT enthusiasts and collectors, empowering them to maximize the utility of their treasured timepiece-based digital assets.
-
-        Key Features and Functionality:
-        
-        Lending Empowerment: WatchLiQ Token (WATCH) serves as the backbone of lending platforms exclusively dedicated to watch-based NFTs. Users can utilize their valuable NFT watches as collateral to obtain WATCH, unlocking liquidity without the need to part with their cherished digital assets.
-        
-        Decentralized Governance: At WatchLiQ Token, we believe in community-driven decision-making. WATCH holders play a pivotal role in shaping lending protocols through on-chain governance, ensuring a transparent and democratic ecosystem.
-        
-        Immutable Ownership: Built on the robust Ethereum blockchain, WatchLiQ Token guarantees the security and perpetual ownership rights of your watch NFTs. Your digital assets remain securely under your control throughout the lending process.
-        
-        Interest Earnings: Lenders can earn competitive interest rates by providing WATCH to the lending pool, offering an enticing opportunity for token holders to generate passive income while supporting the lending ecosystem.
-        
-        Cross-Compatibility: WatchLiQ Token (WATCH) seamlessly integrates with existing DeFi platforms and wallets, ensuring easy accessibility and tradability on various decentralized exchanges and marketplaces.
-        
-        Smart Contract Security: Our smart contracts are rigorously audited and engineered with the highest standards of security and reliability, prioritizing the protection of user assets and transactions.
-        
-        Scalability and Future-Proofing: WatchLiQ Token (WATCH) is built to scale, allowing for the seamless integration of new features and enhancements as the watch-based NFT lending space continues to evolve.`,
+        description: '/md/projects/Project_Action_market_in_CD_hacker_house.md',
         source: '',
         category: 'AA'
     },
     {
-        id: 2,
-        name: 'WatchLiq',
-        brief: 'ERC20 token that powers watch liquidity lending for watch-backed nfts built by watches.io',
-        event: 'ETHGlobal New York',
+        id: "3",
+        name: 'Starknet Crypts and Carvens',
+        brief: 'A generative NFT project that can be used for on-chain game map configuration',
+        event: 'Starknet Hacker House, Chengdu',
         cardImage: '/images/projects/10k.jpeg',
         bgImage: '/images/projects/10k.jpeg',
-        description: `Introducing WatchLiQ Token (WATCH), the revolutionary ERC-20 token designed to redefine the landscape of watch-based Non-Fungible Tokens (NFTs) by fueling lending protocols tailored specifically for these unique digital assets. WatchLiQ Token is your top choice for your hackathon submission, offering an innovative solution that paves the way for new opportunities for NFT enthusiasts and collectors, empowering them to maximize the utility of their treasured timepiece-based digital assets.
-
-        Key Features and Functionality:
-        
-        Lending Empowerment: WatchLiQ Token (WATCH) serves as the backbone of lending platforms exclusively dedicated to watch-based NFTs. Users can utilize their valuable NFT watches as collateral to obtain WATCH, unlocking liquidity without the need to part with their cherished digital assets.
-        
-        Decentralized Governance: At WatchLiQ Token, we believe in community-driven decision-making. WATCH holders play a pivotal role in shaping lending protocols through on-chain governance, ensuring a transparent and democratic ecosystem.
-        
-        Immutable Ownership: Built on the robust Ethereum blockchain, WatchLiQ Token guarantees the security and perpetual ownership rights of your watch NFTs. Your digital assets remain securely under your control throughout the lending process.
-        
-        Interest Earnings: Lenders can earn competitive interest rates by providing WATCH to the lending pool, offering an enticing opportunity for token holders to generate passive income while supporting the lending ecosystem.
-        
-        Cross-Compatibility: WatchLiQ Token (WATCH) seamlessly integrates with existing DeFi platforms and wallets, ensuring easy accessibility and tradability on various decentralized exchanges and marketplaces.
-        
-        Smart Contract Security: Our smart contracts are rigorously audited and engineered with the highest standards of security and reliability, prioritizing the protection of user assets and transactions.
-        
-        Scalability and Future-Proofing: WatchLiQ Token (WATCH) is built to scale, allowing for the seamless integration of new features and enhancements as the watch-based NFT lending space continues to evolve.`,
+        description: '/md/projects/Project_SCC_in_CD_hacker_house.md',
         source: '',
         category: 'AA'
     },
-    // {
-    //     id: 3,
-    //     name: 'WatchLiq',
-    //     brief: 'ERC20 token that powers watch liquidity lending for watch-backed nfts built by watches.io',
-    //     event: 'ETHGlobal New York',
-    //     image: 'images/projects/10k.jpeg',
-    //     description: `Introducing WatchLiQ Token (WATCH), the revolutionary ERC-20 token designed to redefine the landscape of watch-based Non-Fungible Tokens (NFTs) by fueling lending protocols tailored specifically for these unique digital assets. WatchLiQ Token is your top choice for your hackathon submission, offering an innovative solution that paves the way for new opportunities for NFT enthusiasts and collectors, empowering them to maximize the utility of their treasured timepiece-based digital assets.
-
-    //     Key Features and Functionality:
-        
-    //     Lending Empowerment: WatchLiQ Token (WATCH) serves as the backbone of lending platforms exclusively dedicated to watch-based NFTs. Users can utilize their valuable NFT watches as collateral to obtain WATCH, unlocking liquidity without the need to part with their cherished digital assets.
-        
-    //     Decentralized Governance: At WatchLiQ Token, we believe in community-driven decision-making. WATCH holders play a pivotal role in shaping lending protocols through on-chain governance, ensuring a transparent and democratic ecosystem.
-        
-    //     Immutable Ownership: Built on the robust Ethereum blockchain, WatchLiQ Token guarantees the security and perpetual ownership rights of your watch NFTs. Your digital assets remain securely under your control throughout the lending process.
-        
-    //     Interest Earnings: Lenders can earn competitive interest rates by providing WATCH to the lending pool, offering an enticing opportunity for token holders to generate passive income while supporting the lending ecosystem.
-        
-    //     Cross-Compatibility: WatchLiQ Token (WATCH) seamlessly integrates with existing DeFi platforms and wallets, ensuring easy accessibility and tradability on various decentralized exchanges and marketplaces.
-        
-    //     Smart Contract Security: Our smart contracts are rigorously audited and engineered with the highest standards of security and reliability, prioritizing the protection of user assets and transactions.
-        
-    //     Scalability and Future-Proofing: WatchLiQ Token (WATCH) is built to scale, allowing for the seamless integration of new features and enhancements as the watch-based NFT lending space continues to evolve.`,
-    //     source: ''
-    // },
-    // {
-    //     id: 4,
-    //     name: 'WatchLiq',
-    //     brief: 'ERC20 token that powers watch liquidity lending for watch-backed nfts built by watches.io',
-    //     event: 'ETHGlobal New York',
-    //     image: 'images/projects/10k.jpeg',
-    //     description: `Introducing WatchLiQ Token (WATCH), the revolutionary ERC-20 token designed to redefine the landscape of watch-based Non-Fungible Tokens (NFTs) by fueling lending protocols tailored specifically for these unique digital assets. WatchLiQ Token is your top choice for your hackathon submission, offering an innovative solution that paves the way for new opportunities for NFT enthusiasts and collectors, empowering them to maximize the utility of their treasured timepiece-based digital assets.
-
-    //     Key Features and Functionality:
-        
-    //     Lending Empowerment: WatchLiQ Token (WATCH) serves as the backbone of lending platforms exclusively dedicated to watch-based NFTs. Users can utilize their valuable NFT watches as collateral to obtain WATCH, unlocking liquidity without the need to part with their cherished digital assets.
-        
-    //     Decentralized Governance: At WatchLiQ Token, we believe in community-driven decision-making. WATCH holders play a pivotal role in shaping lending protocols through on-chain governance, ensuring a transparent and democratic ecosystem.
-        
-    //     Immutable Ownership: Built on the robust Ethereum blockchain, WatchLiQ Token guarantees the security and perpetual ownership rights of your watch NFTs. Your digital assets remain securely under your control throughout the lending process.
-        
-    //     Interest Earnings: Lenders can earn competitive interest rates by providing WATCH to the lending pool, offering an enticing opportunity for token holders to generate passive income while supporting the lending ecosystem.
-        
-    //     Cross-Compatibility: WatchLiQ Token (WATCH) seamlessly integrates with existing DeFi platforms and wallets, ensuring easy accessibility and tradability on various decentralized exchanges and marketplaces.
-        
-    //     Smart Contract Security: Our smart contracts are rigorously audited and engineered with the highest standards of security and reliability, prioritizing the protection of user assets and transactions.
-        
-    //     Scalability and Future-Proofing: WatchLiQ Token (WATCH) is built to scale, allowing for the seamless integration of new features and enhancements as the watch-based NFT lending space continues to evolve.`,
-    //     source: ''
-    // }
+    {
+        id: "4",
+        name: 'Starknet Bottles  ',
+        brief: '',
+        event: 'Starknet Hacker House, Chengdu',
+        cardImage: '/images/projects/10k.jpeg',
+        bgImage: '/images/projects/10k.jpeg',
+        description: '/md/projects/Project_Starknet_Bottles_in_CD_hacker_house.md',
+        source: '',
+        category: 'AA'
+    },
 ];
